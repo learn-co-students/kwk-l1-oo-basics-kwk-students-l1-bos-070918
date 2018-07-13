f0bd6f3cdff2df1e0dd8c09e6e8843d10dbd7bed
mggbhn <maggiebswim@gmail.com>
2018-07-13 14:23:18 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,23 @@
 # book.rb
 # Add you Book class here
+class Book
+  
+attr_reader :title
+attr_accessor :author, :genre, :page_count
+  
+  def initialize(title)
+    @title = title
+    
+  end
+    
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+
+  end
+end 
+
+# great_gatsby = Book.new("And Then There Were None")
+
+
+# great_gatsby.turn_page
+
